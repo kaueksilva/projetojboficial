505bde4a0b84894c610dac5489321438c07f0516
Kauê Silva <kaueksilva12@gmail.com>
2024-08-23 14:00:47 -0300

    front e back

---
+++ front-oficial/src/app/layout.jsx
@@ -0,0 +1,18 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "./style/footer.css"
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Projeto Jaboatão dos Guararapes",
+  description: "Desenvolvido por Kauê Nascimento",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
