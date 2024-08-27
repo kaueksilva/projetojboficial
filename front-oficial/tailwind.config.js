4a40251e1254ff168ea7013a6f85c2440e5a5065
Larissa Machado <larissadsmachado0@gmail.com>
2024-08-27 13:42:37 -0300

    larissa1

--- front-oficial/tailwind.config.js
+++ front-oficial/tailwind.config.js
@@ -10,5 +10,28 @@ module.exports = {
       
     },
   },
-  plugins: [],
+  plugins: [], 
+  
+  theme: {
+    extend: {
+      keyframes: {
+        buzz: {
+          '0%': { transform: 'translateX(0)' },
+          '10%': { transform: 'translateX(-2px)' },
+          '20%': { transform: 'translateX(2px)' },
+          '30%': { transform: 'translateX(-2px)' },
+          '40%': { transform: 'translateX(2px)' },
+          '50%': { transform: 'translateX(-2px)' },
+          '60%': { transform: 'translateX(2px)' },
+          '70%': { transform: 'translateX(-2px)' },
+          '80%': { transform: 'translateX(2px)' },
+          '90%': { transform: 'translateX(-2px)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        buzz: 'buzz 0.6s ease-in-out',
+      },
+    },
+  },
 };
