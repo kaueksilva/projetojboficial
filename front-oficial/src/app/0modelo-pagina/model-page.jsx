7b0c386646b985f1eb33d5b7d3475edfe0a50ae9
Kauê Silva <kaueksilva12@gmail.com>
2024-09-17 14:16:24 -0300

    att

--- front-oficial/src/app/0modelo-pagina/model-page.jsx
+++ front-oficial/src/app/0modelo-pagina/model-page.jsx
@@ -1,10 +1,11 @@
 import Menu from "../components/Menu";
 import Footer from "../components/Footer";
 
-export default function Model({children, caminhoImagem, titulo, subtitulo }) {
+export default function Model({ children, caminhoImagem, titulo, subtitulo }) {
   return (
     <div>
       <Menu />
+      
       <div className="relative w-full h-[250px] flex items-end justify-center bg-[#2365f3]">
         {/* Imagem de fundo */}
         <img
@@ -16,8 +17,9 @@ export default function Model({children, caminhoImagem, titulo, subtitulo }) {
         <div className="absolute inset-0 bg-black opacity-40"></div>
         {/* Conteúdo sobre a imagem */}
         <div className="z-10 text-center text-white pb-4 ">
-          <h1 className=" text-[41px] font-bold drop-shadow-[0_4px_6px_rgba(0,0,0,0.9)]">{titulo}</h1>
-          {/* Aumenta o título */}
+        <h1 className=" text-[40px] leading-[40px] pb-[5px] font-bold drop-shadow-[0_4px_6px_rgba(0,0,0,0.9)]">{titulo}</h1>
+
+          {/* Aumenta o subtítulo */}
           <p className="text-[14px]">{subtitulo}</p>
         </div>
       </div>
