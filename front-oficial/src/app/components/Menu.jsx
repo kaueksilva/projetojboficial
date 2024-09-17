b8d33619a11736bb7235e0461e7a82f6ecb1d808
Larissa Machado <larissadsmachado0@gmail.com>
2024-09-17 14:24:53 -0300

    okk

--- front-oficial/src/app/components/Menu.jsx
+++ front-oficial/src/app/components/Menu.jsx
@@ -334,7 +334,7 @@ const Menu = () => {
                         )}
                       </li>
                       <li className="hover:bg-[#224276] hover:text-white transition duration-200 border-b-[1px] border-[#c8c8c87c]">
-                        <a href="#" className="block px-4 py-4">
+                        <a href="../desenvolvimento_institucional" className="block px-4 py-4">
                           DESENVOLVIMENTO INSTITUCIONAL
                         </a>
                       </li>
@@ -348,12 +348,12 @@ const Menu = () => {
                         </a>
                       </li>
                       <li className="hover:bg-[#224276] hover:text-white transition duration-200 border-b-[1px] border-[#c8c8c87c]">
-                        <a href="#" className="block px-4 py-4">
+                        <a href="../mulher" className="block px-4 py-4">
                           MULHER
                         </a>
                       </li>
                       <li className="hover:bg-[#224276] hover:text-white transition duration-200">
-                        <a href="#" className="block px-4 py-4">
+                        <a href="../saude" className="block px-4 py-4">
                           SAÚDE
                         </a>
                       </li>
@@ -661,7 +661,7 @@ const Menu = () => {
                         )}
                       </li>
                       <li className="mb-3">
-                        <a href="#" onClick={toggleMenu}>
+                        <a href="../desenvolvimento_institucional" onClick={toggleMenu}>
                           DESENVOLVIMENTO INSTITUCIONAL
                         </a>
                       </li>
@@ -674,12 +674,12 @@ const Menu = () => {
                         </a>
                       </li>
                       <li className="mb-3">
-                        <a href="#" onClick={toggleMenu}>
+                        <a href="../mulher" onClick={toggleMenu}>
                           MULHER
                         </a>
                       </li>
                       <li className="mb-3">
-                        <a href="#" onClick={toggleMenu}>
+                        <a href="../saude" onClick={toggleMenu}>
                           SAÚDE
                         </a>
                       </li>
