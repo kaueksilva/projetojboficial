eb3396627d39cb4616ca32848891059ac7e240c6
Larissa Machado <larissadsmachado0@gmail.com>
2024-11-14 15:20:06 -0300

    carta_de_servico

--- front-oficial/src/app/carta_de_servico/servicos-direitos-humanos/servicos-direitos-humanos.jsx
+++ front-oficial/src/app/carta_de_servico/servicos-direitos-humanos/servicos-direitos-humanos.jsx
@@ -16,37 +16,37 @@ const ServicosDeDireitosHumanos = () => {
               title: 'INTEGRA',
               description:
                 'DADOS GERAIS SETORES PÚBLICOS DÚVIDAS DADOS GERAIS DESCRIÇÃO GERAL: O Programa INTEGRA está vinculado à Secretaria Executiva de Direitos humanos, da Família e de Política',
-              link: '/carta-de-servico/servicos-direitos-humanos/integra'
+              link: '/carta_de_servico/servicos-direitos-humanos/integra'
             },
             {
               title: 'FORTALECENDO LAÇOS',
               description:
                 'DADOS GERAIS SETORES PÚBLICOS DÚVIDAS DADOS GERAIS DESCRIÇÃO GERAL: O Programa Fortalecendo Laços está vinculado à Secretaria Executiva de Direitos Humanos, da Família e de',
-              link: '/carta-de-servico/servicos-direitos-humanos/fortalecendo-lacos'
+              link: '/carta_de_servico/servicos-direitos-humanos/fortalecendo-lacos'
             },
             {
               title: 'COMUNICA LIBRAS',
               description:
                 'DADOS GERAIS SETORES PÚBLICOS DÚVIDAS DADOS GERAIS DESCRIÇÃO GERAL: O Programa Comunica Libras está vinculado à Secretaria Executiva de Direitos Humanos, da Família e de',
-              link: '/carta-de-servico/servicos-direitos-humanos/comunica-libras'
+              link: '/carta_de_servico/servicos-direitos-humanos/comunica-libras'
             },
             {
               title: 'CARTEIRA CIPTEA',
               description:
                 'DADOS GERAIS SETORES PÚBLICOS DÚVIDAS DADOS GERAIS DESCRIÇÃO GERAL: O Programa CIPTEA está vinculado à Secretaria Executiva de Direitos Humanos, da Família e de Política',
-              link: '/carta-de-servico/servicos-direitos-humanos/carteira-ciptea'
+              link: '/carta_de_servico/servicos-direitos-humanos/carteira-ciptea'
             },
             {
               title: 'Balcão da Cidadania',
               description:
                 'DADOS GERAIS SETORES PÚBLICOS DÚVIDAS DADOS GERAIS DESCRIÇÃO GERAL: O Programa Balcão da Cidadania está vinculado à Secretaria Executiva de Direitos Humanos, da Família e',
-              link: '/carta-de-servico/servicos-direitos-humanos/balcao-da-cidadania'
+              link: '/carta_de_servico/servicos-direitos-humanos/balcao-da-cidadania'
             },
             {
               title: 'Núcleo de Apoio a Vítimas de Violência',
               description:
                 'DADOS GERAIS SETORES PÚBLICOS DÚVIDAS DADOS GERAIS DESCRIÇÃO GERAL: O Núcleo de Apoio às Vítimas de Violência (NAVV) está vinculado à Secretaria Executiva de Direitos',
-              link: '/carta-de-servico/servicos-direitos-humanos/nucleo-de-apoio-a-vitimas-de-violencia'
+              link: '/carta_de_servico/servicos-direitos-humanos/nucleo-de-apoio-a-vitimas-de-violencia'
             }
           ].map((item, index) => (
             <div key={index} className="flex items-start mb-4 border-b pb-4">
