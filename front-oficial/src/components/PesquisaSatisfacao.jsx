e253ff17cca5873171617115f1d7e877e424bd42
Larissa Machado <larissadsmachado0@gmail.com>
2024-11-26 09:56:36 -0300

    componente-pesquisa de satisfação - comentada para nao aparecer!!!

--- front-oficial/src/components/PesquisaSatisfacao.jsx
+++ front-oficial/src/components/PesquisaSatisfacao.jsx
@@ -22,33 +22,34 @@ const PesquisaSatisfacao = () => {
   }, []);
 
   return (
-    <div className="max-w-md mx-auto m-8 p-6 border border-gray-300 rounded-lg shadow-lg">
-      <h1 className="text-center text-2xl font-bold text-[#003476] mb-6">Pesquisa de Satisfação</h1>
+    // <div className="max-w-md mx-auto m-8 p-6 border border-gray-300 rounded-lg shadow-lg">
+    //   <h1 className="text-center text-2xl font-bold text-[#003476] mb-6">Pesquisa de Satisfação</h1>
 
-      <label htmlFor="opcao" className="block text-center text-[#00224e] mb-4">Foram informações úteis?</label>
+    //   <label htmlFor="opcao" className="block text-center text-[#00224e] mb-4">Foram informações úteis?</label>
 
-      <select
-        id="opcao"
-        className="w-full p-2 mb-6 border border-gray-300 rounded-lg"
-      >
-        <option>Selecione</option>
-        <option>Não</option>
-        <option>Sim</option>
-      </select>
+    //   <select
+    //     id="opcao"
+    //     className="w-full p-2 mb-6 border border-gray-300 rounded-lg"
+    //   >
+    //     <option>Selecione</option>
+    //     <option>Não</option>
+    //     <option>Sim</option>
+    //   </select>
 
-      <div id="oculto1" className="hidden"></div>
-      <div id="oculto2" className="hidden"></div>
+    //   <div id="oculto1" className="hidden"></div>
+    //   <div id="oculto2" className="hidden"></div>
 
-      <div className="flex justify-center">
-        <button
-          type="submit"
-          id="botao-formulario"
-          className="px-6 py-2 text-white bg-[#014daf] rounded-full transition-colors hover:bg-[#002e6a]"
-        >
-          Enviar
-        </button>
-      </div>
-    </div>
+    //   <div className="flex justify-center">
+    //     <button
+    //       type="submit"
+    //       id="botao-formulario"
+    //       className="px-6 py-2 text-white bg-[#014daf] rounded-full transition-colors hover:bg-[#002e6a]"
+    //     >
+    //       Enviar
+    //     </button>
+    //   </div>
+    // </div>
+    <div></div>
   );
 };
 
