7117e75fce01cdba0595e2e16741ae55c7ca8cfc
Larissa Machado <larissadsmachado0@gmail.com>
2024-12-04 10:01:14 -0300

    ciptea

---
+++ front-oficial/src/app/cidadao/cadastro-da-carteira-de-identificacao-da-pessoa-com-transtorno-do-espectro-do-autismo/page.jsx
@@ -0,0 +1,32 @@
+import Ciptea from "./ciptea.jsx";
+import Model from "../../components/model-page";
+import Link from "next/link";
+
+export const metadata = {
+  title: "CADASTRO DA CARTEIRA DE IDENTIFICAÇÃO DA PESSOA COM TRANSTORNO DO ESPECTRO DO AUTISMO - Prefeitura do Jaboatão dos Guararapes",
+};
+
+export default function config() {
+  return (
+    <div>
+      <Model
+        caminhoImagem={"/images/servicos-Ciptea.jpg"}
+        titulo={"CADASTRO DA CARTEIRA DE IDENTIFICAÇÃO DA PESSOA COM TRANSTORNO DO ESPECTRO DO AUTISMO"}
+        subtitulo={
+          <span>
+            <Link
+              href="../"
+              className="text-white hover:underline hover:text-yellow-500 hover:brightness-125 transition-all"
+            >
+              Início
+            </Link>
+            &nbsp;»{" "}
+            <span className="brightness-125">CADASTRO DA CARTEIRA DE IDENTIFICAÇÃO DA PESSOA COM TRANSTORNO DO ESPECTRO DO AUTISMO</span>
+          </span>
+        }
+      >
+        <Ciptea />
+      </Model>
+    </div>
+  );
+}
